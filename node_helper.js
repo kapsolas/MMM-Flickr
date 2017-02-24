2993a0e76058860298e55a6d251ce992ec41feb4
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-02-24 19:04:21 -0300

    remove extra spaces

--- node_helper.js
+++ node_helper.js
@@ -7,13 +7,13 @@
 
  var NodeHelper = require("node_helper");
  var request = require('request');
- 
+
  module.exports = NodeHelper.create({
     // subclass start method.
     start: function() {
         console.log("Starting node_helper for module [" + this.name + "]");
     },
-    
+
     // subclass socketNotificationReceived
     socketNotificationReceived: function(notification, payload){
         //console.log("=========== notification received: " + notification);
@@ -21,21 +21,21 @@
             this.getImagesFromJSON(payload.url, payload.size);
         }
     },
-    
+
     getImagesFromJSON: function(api_url, size) {
         //console.log('============ HERE =================');
         var self = this;
-        request({url: api_url, method: 'GET'}, function(error, response, body) 
+        request({url: api_url, method: 'GET'}, function(error, response, body)
         {
-            if (!error && response.statusCode == 200) 
+            if (!error && response.statusCode == 200)
             {
                 // get our images out of the FLICKR JSON response
                 var items = JSON.parse(body).items;
-                
-                // create our model, a dictionary with 
+
+                // create our model, a dictionary with
                 var images = {};
                 images.photo = new Array();
-                
+
                 for (var i in items)
                 {
                     var title = items[i].title;
@@ -49,19 +49,19 @@
                     {
                         var media = items[i].media.m;
                     }
-                    
+
                     //console.log("title: " + title + "\nmedia: " + media.m);
-                    
+
                     // create a new array for each images object in the dictionary
                     images.photo.push( {
                         "title" : title,
                         "photolink" : media
                     });
-                    
+
                 }
                 //console.log("count: " + images.photo.length);
                 self.sendSocketNotification('FLICK_IMAGE_LIST', images);
-                
+
             }
             else
             {
